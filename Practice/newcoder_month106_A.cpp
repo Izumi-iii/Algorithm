0cb521442e5f3ebe9e9920d77f1f200cfab32a05
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-11-14 21:27:47 +0800

    Remove unnecessary files and configurations, including executables, IDE settings, and source code from the 小白月赛107 contest directory.

--- Practice/newcoder_month106_A.cpp
+++ Practice/newcoder_month106_A.cpp
@@ -6,23 +6,22 @@
 #include <cmath>
 using namespace std;
 
+int main()
+{
+    int a, b, c, d;
 
-int main() {
-    int a,b,c,d;
+    cin >> a >> b >> c >> d;
 
-    cin>>a>>b>>c>>d;
+    int result = pow(a, b) * c + d;
 
-    int result= pow(a,b)*c+d;
-
-    if(result%2 == 0){
-        cout<<"NO";
-    }else{
-        cout<<"YES";
+    if (result % 2 == 0)
+    {
+        cout << "NO";
     }
-
-
-
-
+    else
+    {
+        cout << "YES";
+    }
 
     return 0;
 }
